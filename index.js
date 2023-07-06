6d4ab565083d50198c17325b32e70a4ecd930b0e
NalinYadav <nalinyadav106@gmail.com>
2023-07-06 07:56:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const images = require("./utils/images");
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+const People = require("./models/People");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
+
+app.get("/", async (req, res) => {
+  const images = await People.find();
+  res.render("home", { images: images });
+});
+
+app.post("/people", async (req, res) => {
+  const data = req.body;
+  try {
+    const people = await People.create(data);
+    res.status(200).json(people);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.post("/submit", async (req, res) => {
+  const { id, userId } = req.body;
+  let updated;
+  if (typeof id == "object") {
+    await People.updateMany({ id: { $in: id } }, { $push: { votes: userId } });
+  } else {
+    await People.findOneAndUpdate(
+      { id },
+      { $push: { votes: userId } },
+      { new: true }
+    );
+  }
+  console.log("updated");
+  res.redirect("/");
+});
+app.listen(3000, () => console.log("port running at server 3000"));
